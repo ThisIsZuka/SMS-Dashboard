1aea46acdf04bac498dd15d6bf04d862dd08b333
Zuka_ <kid00345@hotmail.com>
2023-06-29 13:47:44 +0700

    Change condition sql Convert Qr Barcode

--- app/Http/Controllers/Convert_QR_BarCode.php
+++ app/Http/Controllers/Convert_QR_BarCode.php
@@ -42,10 +42,11 @@ class Convert_QR_BarCode extends BaseController
 
             $data = $request->all();
             $DUE_DATE = $data['DUE_DATE'];
+            $INV_DATE = $data['INV_DATE'];
 
             $TTP_INV_BARCODE = DB::connection('sqlsrv_HPCOM7')->table('dbo.TTP_INV_BARCODE')
                 ->select('SEQ_ID')
-                ->where('DUE_DATE', $DUE_DATE)
+                ->where('INV_DATE', $INV_DATE)
                 ->where('CRT_FLG', '!=' , 'Y')
                 // ->limit(1)
                 ->get();
